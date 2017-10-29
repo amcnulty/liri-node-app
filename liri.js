8cc7712a91786b824bf4eef4d9d3c72fc7ef8362
amcnulty <amcnulty88@swbell.net>
2017-10-28 17:03:19 -0700

    Movies
    The app is currently pulling movie information and printing it to the screen.

--- liri.js
+++ liri.js
@@ -31,15 +31,49 @@ var app = {
         console.log("SPOTIFY!");
     },
     OMDB: function() {
-        console.log("OMDB!!");
+        var movieData;
+        this.requestData(this.buildUri(), function(res) {
+            movieData = res;
+            app.displayMovieInfo(movieData);
+        });
+    },
+    buildUri: function() {
+        if (this.nameValue === undefined) {
+            console.log("\n\nYou have not supplied a name value for this command!\n\nTry <node liri.js -help> for information on using this app.");
+            process.exit();
+        } 
+        else {
+            return 'http://www.omdbapi.com/?apikey=40e9cece&t=' + this.nameValue;
+        }
+    },
+    displayMovieInfo: function(movie) {
+        console.log("\n\nMovie Title: " + movie.Title);
+        console.log("Release Year: " + movie.Year);
+        console.log("IMBD Rating: " + movie.Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating: " + movie.Ratings[1].Value);
+        console.log("Country of production: " + movie.Country);
+        console.log("Languages: " + movie.Language);
+        console.log("Plot: " + movie.Plot);
+        console.log("Actors: " + movie.Actors);
     },
     storedAction: function() {
         console.log("I DON'T GET IT");
     },
     help: function() {
         console.log("HELP ON USING THIS APP!");
+    },
+    requestData: function(uri, callback) {
+        request(uri, function(err, response, body) {
+            if (err) {
+                console.error(err);
+            }
+            else if (response.statusCode === 200) {
+                callback(JSON.parse(body));
+            }
+        })
     }
 }
 
 if (app.command === undefined) console.log("\n\nYou have not specified a command!\n\nTry <node liri.js -help> for information on using this app.");
+else if (app.commandMap[app.command] === undefined) console.log("\n\n" + app.command + " is not a valid command!\n\nTry <node liri.js -help> for information on using this app.");
 else app[app.commandMap[app.command]]();
